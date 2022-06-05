1837b37342a6f19f8e51391837cfd1beccc788c5
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-06-05 10:51:47 -0500

    Ejemplo para bajar un archivo de YouTube

---
+++ YouTube-Downloader/README.md
@@ -0,0 +1,7 @@
+*Fuente:*
+
+[Creating a simple YouTube Video Downloader using Python | Engineering Education (EngEd) Program | Section](https://www.section.io/engineering-education/youtube-video-downloader-using-python/)
+
+[GitHub - Patalin/YouTube-Downloader](https://github.com/Patalin/YouTube-Downloader)
+
+
---
+++ YouTube-Downloader/main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('500x300')
+root.resizable(0, 0)
+root.title("YouTube Downloader")
+root.configure(bg="#f2eee3")
+
+Label(root, text='TouTube Video Downloader', font='arial 20 bold', bg="#f2eee3", fg="red").pack()
+
+link = StringVar()
+Label(root, text='Pegar el link de YouTube aqui:', font='arial 20 bold', bg="#f2eee3", fg="red").place(x=40, y=60)
+link_enter = Entry(root, width=50, textvariable=link).place(x=32, y=90)
+
+def downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text='DESCARGADO', font='arial 15 bold', bg="#f2eee3", fg="red").place(x=185, y=210)
+
+Button(root,
+       text='DOWNLOAD',
+       font='arial 15 bold',
+       bg='pale violet red',
+       padx=2,
+       command=downloader,
+       fg="blue").place(x=180, y=150)
+
+root.mainloop()
